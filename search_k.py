1dc82ca80bb60c1473ac1ecda1342c1ce21430dd
qianxunchen <2511284740@qq.com>
2019-10-18 17:33:48 +0800

    巨恶心的取最小的k个数的方法

---
+++ search_k.py
@@ -0,0 +1,15 @@
+def sort(a):
+    for i in range(1, len(a)):
+        for j in range(0, len(a)-i):
+            if a[j] > a[j+1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    return a
+
+
+
+a = [1,2,4,3,6,5]
+sort(a)
+print(a)
+x = 3
+c = a[:x]
+print(c)
