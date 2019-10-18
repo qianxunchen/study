018decab244dad4269a3f9195a2bec2aa260548e
qianxunchen <2511284740@qq.com>
2019-10-18 16:42:43 +0800

    合并排序

---
+++ merge_sort.py
@@ -0,0 +1,32 @@
+#递归实现合并
+def merge(a, b):
+    c = []
+    h = j = 0
+    while j < len(a) and h < len(b):
+        if a[j] < b[h]:
+            c.append(a[j])
+            j += 1
+        else:
+            c.append(b[h])
+            h += 1
+
+    if j == len(a):
+        for i in b[h:]:
+            c.append(i)
+    else:
+        for i in a[j:]:
+            c.append(i)
+    return c
+
+def merge_sort(lists):
+    #判断列表长度
+    if len(lists) <= 1:
+        return lists
+    mid = len(lists)//2#求列表中间
+    left = merge_sort(lists[:mid])#取左边
+    right = merge_sort(lists[mid:])#取右边
+    return merge(left, right)
+
+
+a = [14, 2, 34, 43, 21, 19]
+print (merge_sort(a))
